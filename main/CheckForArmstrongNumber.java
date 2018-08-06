40a9c4bc1a8ee2192f3270cb9415af780bc7f320
Chilja Speransky <magiclanzelot@gmx.net>
2018-08-06 17:46:25 +0200

    Class refactored for clean code

---
+++ main/CheckForArmstrongNumber.java
@@ -0,0 +1,57 @@
+package main;
+
+import javax.swing.JOptionPane;
+
+/* program to check if a given number is an Armstrong number (a  number, which is the result of multiplying all digits like this: digit^index of the digit)
+ * input is given
+ * output via console
+ */
+
+class CheckForArmstrongNumber {
+	
+	private static CheckForArmstrongNumber number = new CheckForArmstrongNumber();
+	
+	String checkForArmstrongNumber(String enteredNumber) {
+		int[] allDigits = number.splitNumberInSingleDigits(enteredNumber);
+		int result = number.calculateSumToCheck(allDigits);
+		String message = number.returnResultMessageOfCheck(result,enteredNumber);
+		
+		return message;
+	}
+	
+	int[] splitNumberInSingleDigits(String enteredNumber) {
+		int digits[] = new int[enteredNumber.length()];
+		
+		for(int i = 0; i < digits.length; i++) {
+			digits[i] = Integer.parseInt(enteredNumber.substring(i, i+1));
+		}
+		
+		return digits;
+	}
+	
+	int calculateSumToCheck(int[] digits) {
+		int result = 0;
+		
+		for(int i = 0; i < digits.length; i++) {
+			result = result + (int) Math.pow((double) digits[i], (double) digits.length - 0);
+		}
+		
+		return result;
+	}
+	
+	String returnResultMessageOfCheck(int result, String enteredNumber) {		
+		if(result == Integer.parseInt(enteredNumber)) {
+			return "The number " + enteredNumber + " is an armstrong number!";
+		} else {
+			return "The number " + enteredNumber + " is not armstrong number, the result is: " + result + ".";
+		}
+	}
+	
+	public static void main(String[] args) {	
+		String enteredNumber = JOptionPane.showInputDialog("Enter a number to check if it is an Armstrong Number:");
+		String resultMessage = number.checkForArmstrongNumber(enteredNumber);
+		
+		JOptionPane.showMessageDialog(null, resultMessage);
+	}
+
+}
