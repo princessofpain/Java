dcb495f5bf55bf105823d6d0164febe0bebf47cf
princessOfPain <magiclanzelot@gmx.net>
2018-08-08 15:18:40 +0200

    tests updated for refactored code

--- test/ConvertNameToAcronymTest.java
+++ test/ConvertNameToAcronymTest.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import main.ConvertNameToAcronym;
@@ -13,18 +14,66 @@ class ConvertNameToAcronymTest {
 	@Test 
 	void nameIsNotConvertedToNull() {
 		String nameToConvert = "Heidi Müller";
-		String actualAcronymResult = name.isConvertedToAcronym(nameToConvert);
+		String actualResult = name.convert(nameToConvert);
 
-		assertNotNull(actualAcronymResult);
+		assertNotNull(actualResult);
 	}
 	
 	@Test
 	void acronymHasAllStartingLetters() {
 		String nameToConvert = "Heidi Müller";
-		String actualAcronymResult = name.isConvertedToAcronym(nameToConvert);		
-		String expectedAcronymResult = "HM";
+		String actualResult = name.convert(nameToConvert);		
+		String expectedResult = "HM";
 		
-		assertEquals(expectedAcronymResult, actualAcronymResult);
+		assertEquals(expectedResult, actualResult);
 	}
 
+	@Nested
+	class checkInputValidity {
+		
+		@Test
+		void isNull() {
+			String nameToConvert = null;
+			String actualResult = name.convertNameIfCheckIsPassed(nameToConvert);
+			String expectedResult = "This is not a correct name. Please enter a name and a surname. Leave out name suffixes.";
+			
+			assertEquals(expectedResult, actualResult);
+		}
+		
+		@Test
+		void hasZeroLength() {
+			String nameToConvert = "";
+			String actualResult = name.convertNameIfCheckIsPassed(nameToConvert);
+			String expectedResult = "This is not a correct name. Please enter a name and a surname. Leave out name suffixes.";
+			
+			assertEquals(expectedResult, actualResult);
+		}
+		
+		@Test
+		void isNotNumeric() {
+			String nameToConvert = "123";
+			String actualResult = name.convertNameIfCheckIsPassed(nameToConvert);
+			String expectedResult = "This is not a correct name. Please enter a name and a surname. Leave out name suffixes.";
+			
+			assertEquals(expectedResult, actualResult);
+		}
+		
+		@Test
+		void hasNameAndSurname() {
+			String nameToConvert = "";
+			String actualResult = name.convertNameIfCheckIsPassed(nameToConvert);
+			String expectedResult = "This is not a correct name. Please enter a name and a surname. Leave out name suffixes.";
+			
+			assertEquals(expectedResult, actualResult);
+		}
+		
+		@Test
+		void hasLessThanTwoSpaces() {
+			String nameToConvert = "Barbara de Greus";
+			String actualResult = name.convertNameIfCheckIsPassed(nameToConvert);
+			String expectedResult = "This is not a correct name. Please enter a name and a surname. Leave out name suffixes.";
+			
+			assertEquals(expectedResult, actualResult);
+		}
+	}
 }
