f51160d1419df763fb144f0e52223f7abe10a3b8
Chilja Speransky <magiclanzelot@gmx.net>
2018-11-06 15:48:58 +0100

    Unit tests implemented

---
+++ test/DifferenceOfSquaresTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import main.DifferenceOfSquares;
+
+public class DifferenceOfSquaresTest {
+	DifferenceOfSquares difference = new DifferenceOfSquares();
+	int number = 18;
+	
+	@Test
+	public void squareOfSumIsCorrect() {
+		int expected = 29241;
+		int actual = difference.calculateSquareOfSum(number);
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test 
+	public void sumOfSquaresIsCorrect() {
+		int expected = 2109;
+		int actual = difference.calculateSumOfSquares(number);
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void differenceIsCorrect() {
+		int expected = 27132;
+		int actual = difference.differenceOfResults(number);
+		
+		assertEquals(expected, actual);
+	}
+}
