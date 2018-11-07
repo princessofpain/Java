8eb4f843af46146e19615578db78f2b19a2e65f1
Chilja Speransky <magiclanzelot@gmx.net>
2018-11-07 14:31:11 +0100

    Basic changes for the tests (organizational)

--- main/Hamming.java
+++ main/Hamming.java
@@ -13,10 +13,19 @@ import javax.swing.JOptionPane;
  * dna2 = "CATCGTAATGACGGCCT";
  * */
  
-class Hamming {	
+public class Hamming {	
 	
-	void compare(String dna1, String dna2) {
-		if(handleErrors(dna1, dna2)) return;
+	public static void main(String args[]) {
+		String dna1 = JOptionPane.showInputDialog(null, "Enter the first DNA String:");
+		String dna2 = JOptionPane.showInputDialog(null, "Enter the second DNA String:");
+		
+		Hamming dnaComparison = new Hamming();		
+		String result = dnaComparison.compare(dna1, dna2);
+		dnaComparison.showResultMessage(dna1, dna2, result);
+	}
+	
+	public String compare(String dna1, String dna2) {
+		if(handleErrors(dna1, dna2)) return null;
 		
 		StringBuilder result = new StringBuilder();
 		
@@ -29,10 +38,10 @@ class Hamming {
 		}
 		
 		String resultString = result.toString();
-		showResultMessage(dna1, dna2, resultString);
+		return resultString;
 	}
 	
-	boolean handleErrors(String dna1, String dna2) {
+	public boolean handleErrors(String dna1, String dna2) {
 		if(dna1.length() != dna2.length()) {
 			showErrorMessage();
 			return true;
@@ -49,14 +58,6 @@ class Hamming {
 	void showResultMessage(String dna1, String dna2, String result) {
 		JFrame frame = new JFrame();
 		JOptionPane.showMessageDialog(frame, dna1 + "\n" + dna2 + "\n" + result);
-	}
-	
-	public static void main(String args[]) {
-		String dna1 = JOptionPane.showInputDialog(null, "Enter the first DNA String:");
-		String dna2 = JOptionPane.showInputDialog(null, "Enter the second DNA String:");
-		
-		Hamming dnaComparison = new Hamming();		
-		dnaComparison.compare(dna1, dna2);
 	}
 }
 
