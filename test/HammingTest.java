d4f7002ba29cc2af337e75bfbab3cbeefac4ee57
Chilja Speransky <magiclanzelot@gmx.net>
2018-11-07 14:30:30 +0100

    Unit tests added

---
+++ test/HammingTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import main.Hamming;
+
+public class HammingTest {
+	Hamming h = new Hamming();
+	
+	@Test
+	public void dnaIsNotTheSame() {
+		String dna1 = "ABC";
+		String dna2 = "EFG";
+		
+		String expected = "^ ^ ^ ";
+		String actual = h.compare(dna1, dna2);
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void dnaIsTheSame() {
+		String dna1 = "ABC";
+		String dna2 = "ABC";
+		
+		String expected = "      ";
+		String actual = h.compare(dna1, dna2);
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void stringsHaveDifferentLength() {
+		String dna1 = "ABC";
+		String dna2 = "DEFGHI";
+		
+		String expected = null;
+		String actual = h.compare(dna1, dna2);
+		
+		assertEquals(expected, actual);
+	}
+}
