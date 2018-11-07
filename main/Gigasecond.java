f00799317fa36c792617d973785be9ec45dffe46
Chilja Speransky <magiclanzelot@gmx.net>
2018-11-07 11:25:24 +0100

    Structure was changed to make the code testable

--- main/Gigasecond.java
+++ main/Gigasecond.java
@@ -4,22 +4,37 @@ import java.text.DecimalFormat;
 import javax.swing.JOptionPane;
 
 /* program to calculate the lifetime after 1 Gigasecond which is 10^9
- * output via console and JOptionPane
+ * output via JOptionPane
  * */
 
 public class Gigasecond {
 	
 	public static void main(String args[]) {
-		double liveForAGigasecondInMinutes = (Math.pow(10, 9) / 60);
-		double liveForAGigasecondInHours = Math.pow(10, 9) / (60 * 60);
-		double liveForAGigasecondInDays = Math.pow(10, 9) / (60 * 60 * 24);
-		
+		Gigasecond my = new Gigasecond();
 		DecimalFormat df = new DecimalFormat("0.00");
 		
-		System.out.println("When your lifetime reaches one gigasecond you lived for: \n    " + df.format(liveForAGigasecondInMinutes) + 
-				" minutes or\n    " + df.format(liveForAGigasecondInHours) + " hours or \n    " + df.format(liveForAGigasecondInDays) + " days");
-		
-		JOptionPane.showMessageDialog(null,"When your lifetime reaches one gigasecond you lived for: \n    " + df.format(liveForAGigasecondInMinutes) + 
-				" minutes or\n    " + df.format(liveForAGigasecondInHours) + " hours or \n    " + df.format(liveForAGigasecondInDays) + " days");
+		JOptionPane.showMessageDialog(null,"When your lifetime reaches one gigasecond you lived for: \n    " + df.format(my.liveForAGigasecodInMinutes()) + 
+				" minutes or\n    " + df.format(my.liveForAGigasecodInHours()) + " hours or \n    " + df.format(my.liveForAGigasecodInDays()) + " days or \n    " + 
+				df.format(my.liveForAGigasecodInYears()) + " years.");
+	}
+	
+	public double liveForAGigasecodInMinutes() {
+		double lifetime = Math.pow(10, 9) / 60;
+		return lifetime;
+	}
+	
+	public double liveForAGigasecodInHours() {
+		double lifetime = Math.pow(10, 9) / (60 * 60);
+		return lifetime;
+	}
+	
+	public double liveForAGigasecodInDays() {
+		double lifetime = Math.pow(10, 9) / (60 * 60 * 24);
+		return lifetime;
+	}
+	
+	public double liveForAGigasecodInYears() {
+		double lifetime = Math.pow(10, 9) / (60 * 60 * 24 * 12);
+		return lifetime;
 	}
 }
