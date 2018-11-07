ab280c4d8846d610df622a927e54cc1f1cac642e
Chilja Speransky <magiclanzelot@gmx.net>
2018-11-07 14:18:57 +0100

    Structure changed to make the code testable

--- main/Hamming.java
+++ main/Hamming.java
@@ -6,50 +6,57 @@ import javax.swing.JOptionPane;
 /* program to compare two DNA strings and mark the differences
  * output via console and JOptionPane
  * input via JOption Pane
+ * differences of the Strings are marked with a "^"
+ * similarities are not marked
  * Strings for the dna:
  * dna1 = "GAGCCTACTAACGGGAT";
  * dna2 = "CATCGTAATGACGGCCT";
  * */
  
 class Hamming {	
-	void getDNA() {
-		String dna1 = JOptionPane.showInputDialog(null, "Enter the first DNA String:");
-		String dna2 = JOptionPane.showInputDialog(null, "Enter the second DNA String:");
-		
-		try {
-			compareDNA(dna1, dna2);
-		} catch(StringIndexOutOfBoundsException exc) {
-			JFrame frame = new JFrame("JOptionPane");
-			JOptionPane.showMessageDialog(frame, "The strings have different lengths!", "Error", 0);
-		}
-	}
 	
-	void compareDNA(String dna1, String dna2) {
-		StringBuilder result = new StringBuilder();
-		System.out.println(dna1);
-		System.out.println(dna2);
+	void compare(String dna1, String dna2) {
+		if(handleErrors(dna1, dna2)) return;
 		
-		for(int i = 0; i < dna1.length(); i++) {
-			char letter = dna1.charAt(i);
-			char letter2 = dna2.charAt(i);
-			
-			if(letter == letter2) {
+		StringBuilder result = new StringBuilder();
+		
+		for(int i = 0; i < dna1.length(); i++) {			
+			if(dna1.charAt(i) == dna2.charAt(i)) {
 				result.append("  ");
-				System.out.print(" ");
-				
 			} else {
 				result.append("^ ");
-				System.out.print("^");
 			}
 		}
 		
+		String resultString = result.toString();
+		showResultMessage(dna1, dna2, resultString);
+	}
+	
+	boolean handleErrors(String dna1, String dna2) {
+		if(dna1.length() != dna2.length()) {
+			showErrorMessage();
+			return true;
+		} else {
+			return false;	
+		}
+	}
+	
+	void showErrorMessage() {
+		JFrame frame = new JFrame("JOptionPane");
+		JOptionPane.showMessageDialog(frame, "The strings have different lengths!", "Error", 0);
+	}
+	
+	void showResultMessage(String dna1, String dna2, String result) {
 		JFrame frame = new JFrame();
 		JOptionPane.showMessageDialog(frame, dna1 + "\n" + dna2 + "\n" + result);
 	}
 	
 	public static void main(String args[]) {
+		String dna1 = JOptionPane.showInputDialog(null, "Enter the first DNA String:");
+		String dna2 = JOptionPane.showInputDialog(null, "Enter the second DNA String:");
+		
 		Hamming dnaComparison = new Hamming();		
-		dnaComparison.getDNA();
+		dnaComparison.compare(dna1, dna2);
 	}
 }
 
