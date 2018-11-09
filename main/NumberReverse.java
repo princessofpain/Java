2fef6c02c9abee71b96e871e49d69929e28b1cfd
Chilja Speransky <magiclanzelot@gmx.net>
2018-11-09 13:57:30 +0100

    Structure refactored, code is testable now

--- main/NumberReverse.java
+++ main/NumberReverse.java
@@ -7,28 +7,43 @@ package main;
  */
 
 class NumberReverse {
+	
 	public static void main (String args[]) {
-		int num = 123456;
-		int offset = 0;
-		int i;
+		int num = 123456;		
+		NumberReverse number = new NumberReverse();
 		
-		// change the int to be a string
-		String strNum = Integer.toString(num);
+		number.reverseStringAndPrintInformation(num);
+	}
+	
+	public void reverseStringAndPrintInformation(int num) {
+		String str = Integer.toString(num);
 		
-		StringBuilder strRev = new StringBuilder("");
-		
-		System.out.println("String before: " + strNum);
-		System.out.println("String has " + strNum.length() + " letters");
+		printOriginalStringInformation(str);
+		String reversedStr = reverseString(str);
+		printReversedStringInformation(reversedStr);
+	}
+	
+	void printOriginalStringInformation(String str) {
+		System.out.println("String before: " + str);
+		System.out.println("The original String has " + str.length() + " letters.");
 		System.out.println();
+	}
+	
+	public String reverseString(String str) {
+		StringBuilder strRev = new StringBuilder("");
+		int offset = 0;
 		
-		for(i = strNum.length()-1; i >= 0; i--) {
-			strRev.insert(offset, strNum.charAt(i));
+		for(int i = str.length()-1; i >= 0; i--) {
+			strRev.insert(offset, str.charAt(i));
 			offset++;
 		}
 		
-		// convert the reversed string back to an int
-		int numRev = Integer.parseInt(strRev.toString());
-		
+		return strRev.toString();
+	}
+	
+	void printReversedStringInformation(String reversedStr) {
+		int numRev = Integer.parseInt(reversedStr.toString());		
 		System.out.println("String afterwards: " + numRev);
+		System.out.println("The reversed String has " + reversedStr.length() + " letters.");
 	}
 }
