b3e5b62f70f01238b615a7e21a211f28957eafa7
Chilja Speransky <magiclanzelot@gmx.net>
2018-11-09 09:54:43 +0100

    Refactored

--- main/Matrix.java
+++ main/Matrix.java
@@ -7,32 +7,55 @@ package main;
 public class Matrix {
 	
 	public static void main(String[] args) {
+		Matrix ma = new Matrix();
 		String str = "9 8 7\n5 3 2\n6 6 7";
-		StringBuilder strBuild = new StringBuilder();
-		char[][] matrix = new char[3][3];
+		
+		ma.printOriginalString(str);
+		ma.buildMatrixArray(str);
+	}
+	
+	void printOriginalString(String str) {
 		System.out.println(str);
 		System.out.println();
+	}
+	
+	void buildMatrixArray(String str) {
+		StringBuilder cleanedUpString = removeAllUnecessaryCharacters(str);
+		char[][] matrix = createMatrixArray(cleanedUpString);
+		printMatrixArray(matrix);
+	}
+	
+	StringBuilder removeAllUnecessaryCharacters(String str) {
+		StringBuilder strBuild = new StringBuilder();
 		
-		// remove all unnecessary characters of the string
 		for(int i = 0; i < str.length(); i++) {
 			if(str.charAt(i) >= '1' && str.charAt(i) <= '9')
 				strBuild.append(str.charAt(i));
 		}
 		
+		return strBuild;
+	}
+	
+	char[][] createMatrixArray(StringBuilder cleanedUpInput) {
+		char[][] matrix = new char[3][3];
 		
 		for(int i = 0; i < 3; i++) {
 			for(int row = 0; row < 3; row++) {
-				matrix[i][row] = strBuild.charAt(0);
-				strBuild.deleteCharAt(0);
+				matrix[i][row] = cleanedUpInput.charAt(0);
+				cleanedUpInput.deleteCharAt(0);
 			}
 		}
 		
+		return matrix;
+	}
+	
+	void printMatrixArray(char[][] matrix) {
+		
 		for(char[] row : matrix) {
-			for(char digit : row) {
-				System.out.print(digit + " ");
+			for(char letter : row) {
+				System.out.print(letter + " ");
 			}
 			System.out.println();
 		}
 	}
-
 }
