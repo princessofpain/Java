13d997a96f76acfcff834ef1de1ae8406ea5aa74
Chilja Speransky <magiclanzelot@gmx.net>
2018-11-13 16:39:08 +0100

    ProteinTranslation refactored for adding tests

--- main/ProteinTranslation.java
+++ main/ProteinTranslation.java
@@ -10,61 +10,85 @@ import javax.swing.JOptionPane;
 
 public class ProteinTranslation {
 	
-	String RNAToProteins(String str) {
-		StringBuilder rna = new StringBuilder(str);
-		StringBuilder proteins = new StringBuilder(" ");
-		StringBuilder protein = new StringBuilder();
-		
-		proteinetranslation:
-		for(int i = 0; i <= (str.length() / 3); i++) {
-			
-			for(int a = 0; a < 3; a++) {
-				protein.append(rna.charAt(a));
-			}
-			
-			switch(protein.toString()) {
-			case "AUG": proteins.append("Methionine");
-						break;
-			case "UUU": 
-			case "UUC": proteins.append("Phenylalanine");
-						break;
-			case "UUA":
-			case "UUG": proteins.append("Leucine");
-						break;
-			case "UCU":
-			case "UCC":
-			case "UCA":
-			case "UCG": proteins.append("Serine");
-						break;
-			case "UAU":
-			case "UAC": proteins.append("Tyrosine");
-						break;
-			case "UGU":
-			case "UGC": proteins.append("Cysteine");
-						break;
-			case "UGG": proteins.append("Tryptophan");
-						break;
-			case "UAA":
-			case "UAG":
-			case "UGA": proteins.append("STOP");
-						break proteinetranslation;
-			}
-			
-			if(i < (str.length() / 3)) 
-				proteins.append(", ");
-			
-			rna.delete(0, 3);
-			protein.delete(0, 3);
-		}
-		
-		return proteins.toString();
-	}
-
 	public static void main(String[] args) {
-		String input = JOptionPane.showInputDialog("Enter a rna sequence:");
+		String input = JOptionPane.showInputDialog("Enter a RNA sequence:");
 		ProteinTranslation sequence = new ProteinTranslation();
 		
 		JOptionPane.showMessageDialog(null, "The proteins of the sequence " + input + " are:" + sequence.RNAToProteins(input));
 	}
 	
+	public String RNAToProteins(String str) {
+		StringBuilder allProteins = new StringBuilder(" ");
+		boolean stop = false;
+		
+		for(int i = 0; i < str.length() && !stop; i += 3) {
+			StringBuilder protein = isolateSingleProtein(str, i);
+			allProteins = addProtein(allProteins, protein.toString());		
+			stop = checkForStop(allProteins);
+			allProteins = appendComma(allProteins, stop);
+		}
+		
+		return allProteins.toString();
+	}
+	
+	public StringBuilder isolateSingleProtein(String str, int i) {
+		StringBuilder singleProtein = new StringBuilder();
+		
+		for(int a = 0; a < 3; a++) {
+			singleProtein.append(str.charAt(i + a));
+		}
+		
+		return singleProtein;
+	}
+	
+	public StringBuilder addProtein(StringBuilder allProteins, String protein) {
+		
+		switch(protein) {
+			case "AUG": allProteins.append("Methionine");
+						break;
+			case "UUU": 
+			case "UUC": allProteins.append("Phenylalanine");
+						break;
+			case "UUA":
+			case "UUG": allProteins.append("Leucine");
+						break;
+			case "UCU":
+			case "UCC":
+			case "UCA":
+			case "UCG": allProteins.append("Serine");
+						break;
+			case "UAU":
+			case "UAC": allProteins.append("Tyrosine");
+						break;
+			case "UGU":
+			case "UGC": allProteins.append("Cysteine");
+						break;
+			case "UGG": allProteins.append("Tryptophan");
+						break;
+			case "UAA":
+			case "UAG":
+			case "UGA": allProteins.append("STOP");
+						break;
+			default: 	allProteins.append("STOP, this is not a valid RNA Sequence!");
+						break;
+		}
+		
+		return allProteins;
+	}
+	
+	boolean checkForStop(StringBuilder allProteins) {
+		if(allProteins.toString().contains("STOP")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	StringBuilder appendComma(StringBuilder allProteins, boolean stop) {
+		if(!stop) {
+			allProteins.append(", ");	
+		}
+		
+		return allProteins;
+	}
 }
