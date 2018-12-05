b7c1ff68374440e76d86b4d98139b847e5442e28
Chilja Speransky <magiclanzelot@gmx.net>
2018-12-05 12:14:31 +0100

    ProteinTranslation unit tests added

--- test/ProteinTranslationTest.java
+++ test/ProteinTranslationTest.java
@@ -12,10 +12,30 @@ public class ProteinTranslationTest {
 	
 	@Test
 	public void sequencesAndStopAreIdentified() {
-		String str = "";
-		String expected = "";
-		String actual = sequence.RNAToProteins(str);
+		String str = "UCAUGGUUUUCCUAUUGA";
+		String expected = "Serine, Tryptophan, Phenylalanine, Serine, Tyrosine, STOP";
+		String actual = sequence.toProteins(str);
 		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void firstProteinIsIsolated() {
+		String str = "UCAUGGUUUUCCUAUUGA";
+		int index = 0;
+		String expected = "UCA";
+		String actual = sequence.isolateSingleProtein(str, index).toString();
+				
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void secondProteinIsIsolated() {
+		String str = "UCAUGGUUUUCCUAUUGA";
+		int index = 3;
+		String expected = "UGG";
+		String actual = sequence.isolateSingleProtein(str, index).toString();
+				
 		assertEquals(expected, actual);
 	}
 }
