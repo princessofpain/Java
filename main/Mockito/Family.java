332ecf02cf8123bc17136ebe4f4506977458ee6d
Chilja Speransky <magiclanzelot@gmx.net>
2018-12-20 13:19:22 +0100

    Constructor added

--- main/Mockito/Family.java
+++ main/Mockito/Family.java
@@ -5,24 +5,33 @@ public class Family {
 	private int noOfKids;
 	private int noOfSiblings;
 	
-	public boolean isMarried() {
-		return isMarried;
-	}
-	public void setMarried(boolean isMarried) {
+	public Family(boolean isMarried, int noOfKids, int noOfSiblings) {
 		this.isMarried = isMarried;
-	}
-	public int getNoOfKids() {
-		return noOfKids;
-	}
-	public void setNoOfKids(int noOfKids) {
 		this.noOfKids = noOfKids;
-	}
-	public int getNoOfSiblings() {
-		return noOfSiblings;
-	}
-	public void setNoOfSiblings(int noOfSiblings) {
 		this.noOfSiblings = noOfSiblings;
 	}
 	
+	public boolean isMarried() {
+		return isMarried;
+	}
 	
+	public void setMarried(boolean isMarried) {
+		this.isMarried = isMarried;
+	}
+	
+	public int getNoOfKids() {
+		return noOfKids;
+	}
+	
+	public void setNoOfKids(int noOfKids) {
+		this.noOfKids = noOfKids;
+	}
+	
+	public int getNoOfSiblings() {
+		return noOfSiblings;
+	}
+	
+	public void setNoOfSiblings(int noOfSiblings) {
+		this.noOfSiblings = noOfSiblings;
+	}
 }
