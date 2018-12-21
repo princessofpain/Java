4b63a0f8a8df6dee139e5cb730a63a1b69f4743e
Chilja Speransky <magiclanzelot@gmx.net>
2018-12-21 11:45:59 +0100

    Test for the checkExperience class added

--- test/MockitoTest.java
+++ test/MockitoTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -13,23 +14,40 @@ import main.Mockito.CheckExperience;
 import main.Mockito.DegreeType;
 import main.Mockito.Education;
 import main.Mockito.PersonalInformation;
+import main.Mockito.Profession;
 
 class MockitoTest {
-
+	
+	PersonalInformation pI = Mockito.mock(PersonalInformation.class);
+	CheckExperience check = new CheckExperience();
+	
 	@Test
 	void highestDegreeIsFound() {
-		PersonalInformation pI = Mockito.mock(PersonalInformation.class);
 		Education degree1 = new Education(DegreeType.DOCTORAL, 1.0, "Harvard University", 2002, 2007);
 		Education degree2 = new Education(DegreeType.MASTER, 1.0, "Harvard University", 1999, 2002);
 		ArrayList<Education> allDegrees = new ArrayList<Education>();
 		allDegrees.addAll(Arrays.asList(degree1, degree2));
+		
 		when(pI.getEducation()).thenReturn(allDegrees);
 		
 		DegreeType expected = DegreeType.DOCTORAL;
-		CheckExperience check = new CheckExperience();
 		DegreeType actual = check.getHighestDegree(pI.getEducation()).getDegree();
 		
 		assertEquals(expected, actual);
 	}
-
+	
+	@Test
+	void lastJobIsFound() {
+		Profession job1 = new Profession("Housekeeper", "Adlon Hotel", 2010, 2016);
+		Profession job2 = new Profession("Bartender", "The Roots", 2016, 2018);
+		ArrayList<Profession> allJobs = new ArrayList<Profession>();
+		allJobs.addAll(Arrays.asList(job1, job2));
+		
+		when(pI.getProfession()).thenReturn(allJobs);
+		
+		Profession expected = job2;
+		Profession actual = check.getLastJob(allJobs);
+		
+		assertEquals(expected, actual);
+	}
 }
